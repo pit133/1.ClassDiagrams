62043e45ef0be158bebe52bc6d2ae5481fc9ca86
pit133 <117358265+pit133@users.noreply.github.com>
2024-03-03 16:02:02 +0700

    Добавил класс Account

--- Program.cs
+++ Program.cs
@@ -7,6 +7,45 @@ namespace DZ1
         void Notify(decimal balance);
     }
 
+    class Account
+    {
+        private decimal _balance;
+        List<INotifier> _notifyers;
+
+        public Account()
+        {
+            _balance = 0;
+            _notifyers = new List<INotifier>();
+        }
+        public Account(decimal initialBalance)
+        {
+            _balance = initialBalance;
+            _notifyers = new List<INotifier>();
+        }
+
+        private decimal Balance { get { return _balance; } }
+
+        public void AddNotifier(INotifier notifyer)
+        {
+            _notifyers.Add(notifyer);
+        }
+
+        private void Notificatoin()
+        {
+            foreach (var item in _notifyers)
+            {
+                item.Notify(_balance);
+            }
+        }
+
+        private void ChangeBalance(decimal newBalance)
+        {
+            _balance = newBalance;
+            Notificatoin();
+        }
+        
+    }
+
 
     internal class Program
     {
