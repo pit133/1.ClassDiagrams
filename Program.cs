a443a14ffe4461363aab9a43cb87d2146fcdfcc8
pit133 <117358265+pit133@users.noreply.github.com>
2024-03-03 15:40:02 +0700

    Добавил interface INotifyer

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
-﻿namespace DZ1
+﻿using System.Security.Principal;
+
+namespace DZ1
 {
+    interface INotifier
+    {
+        void Notify(decimal balance);
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
